b40652273f97f23a7edd32d8396e8ac18374dc03
EveKoo <1679789437@qq.com>
2025-08-08 02:05:02 +0800

    fix: 修改路由模式为Hash模式以适配GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 // src/router/index.js
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useUserStore } from '@/stores/user'
 import { useAppStore } from '@/stores/app'
 import swiftRoutes from './swiftRouter'
@@ -16,7 +16,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 });
 
