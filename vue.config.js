5a249c159de922fc0771fc39c612eb9857975189
EveKoo <1679789437@qq.com>
2025-08-08 00:07:05 +0800

    fix: 修复Vercel部署配置

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/swift-share/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   outputDir: 'dist',
   assetsDir: 'static',
   productionSourceMap: false,
